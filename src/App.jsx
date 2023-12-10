9def52e08f54651d36f47da0aae87522ceaa5584
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-12-10 20:51:21 +0700

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
+import { useState } from "react";
 import NewProject from "./component/NewProject";
+import NoProjectSelected from "./component/NoProjectSelected";
 import ProjectSidebar from "./component/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+
+  function handleStart() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProject: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectState);
+
+  let content;
+
+  if (projectState.selectedProject === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStart} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStart} />
+      {content}
     </main>
   );
 }
